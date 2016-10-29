c97a96b102cdb86be27ce51184ac2ce724212051
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2016-10-29 12:48:33 +0200

    Add image resize filter.

--- posts.rss.php
+++ posts.rss.php
@@ -4,7 +4,7 @@
   <item>
     <title><?php echo $post->title; ?></title>
     <description><![CDATA[
-    <?php echo $this->filter($post, ['noTitle', 'brief', 'linksAbsolute']); ?>
+    <?php echo $this->filter($post, ['noTitle', 'brief', 'imageSize(480, 360, 99, true)', 'linksAbsolute']); ?>
 ]]></description>
     <link><?php echo $this->url($post); ?></link>
     <pubDate><?php echo date('r', $post->published); ?></pubDate>
