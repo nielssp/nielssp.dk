56042df7833d0352e83b0d3fe866ab5a59783814
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2016-10-29 20:55:15 +0200

    Fix RSS.

--- _layout.rss.php
+++ _layout.rss.php
@@ -6,11 +6,9 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';
 
 <channel>
   <title><?php echo $config['title']; ?></title>
-  <atom:link href="<?php echo $this->url('/'); ?>" rel="self" type="application/rss+xml" />
+  <atom:link href="<?php echo $this->url(); ?>" rel="self" type="application/rss+xml" />
   <link><?php echo $this->url('/'); ?></link>
-  <?php if (!empty($config['subtitle'])): ?>
-  <description><?php echo $config['subtitle']; ?></description>
-  <?php endif; ?>
+  <description><?php echo $config['description']; ?></description>
 <?php if (isset($lastBuildDate)): ?>
   <lastBuildDate><?php echo date('r', $lastBuildDate); ?></lastBuildDate>
 <?php endif; ?>
