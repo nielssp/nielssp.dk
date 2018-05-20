c5a03873a2a9992592b9e820b76c5aeb5195c146
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2018-05-20 18:22:28 +0200

    New design.

---
+++ site/js/cookie-consent.js
@@ -0,0 +1,24 @@
+if (!document.cookie.split(';').filter(function(item) {
+  return item.indexOf('cookieconsent=') >= 0
+}).length) {
+  var d = document.createElement('div');
+  d.id = 'cookie-consent';
+  document.body.appendChild(d);
+  var t = document.createElement('div');
+  t.innerHTML = 'My website uses cookies.';
+  d.appendChild(t);
+  var link = document.createElement('a');
+  link.href = 'https://cookiesandyou.com/';
+  link.target = '_blank';
+  link.innerHTML = 'Learn more';
+  d.appendChild(link);
+  var b = document.createElement('button');
+  b.innerHTML = 'Dismiss';
+  d.appendChild(b);
+  b.onclick = function () {
+    document.body.removeChild(d);
+    var date = new Date();
+    date.setTime(date.getTime() + 365 * 24 * 60 * 60 * 1000);
+    document.cookie ='cookieconsent=1;path=/;expires=' + date.toGMTString();
+  };
+}
